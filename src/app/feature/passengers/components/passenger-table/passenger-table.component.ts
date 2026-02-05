b16b7aa79865b2e164b7214c9f22c01fb4375654
Bogdan <bogdan.bala@bmwtechworks.ro>
2026-02-05 19:20:23 +0200

    pagination

--- src/app/feature/passengers/components/passenger-table/passenger-table.component.ts
+++ src/app/feature/passengers/components/passenger-table/passenger-table.component.ts
@@ -10,4 +10,36 @@ import { TITANIC_PASSENGERS } from '../../../../shared/titanic-data';
 })
 export class PassengerTableComponent {
   public passengers: PassengerData[] = TITANIC_PASSENGERS;
+  public currentPage: number = 1;
+  public itemsPerPage: number = 50;
+
+  get visiblePassengers(): PassengerData[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.passengers.slice(startIndex, endIndex);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.passengers.length / this.itemsPerPage);
+  }
+
+  nextPage(): void {
+    if (this.currentPage + 1 <= this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage - 1 >= 1) {
+      this.currentPage--;
+    }
+  }
+
+  firstPage(): void {
+    this.currentPage = 1;
+  }
+
+  lastPage(): void {
+    this.currentPage = this.totalPages;
+  }
 }
